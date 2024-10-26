4147bcdfca39ee9a07e91498d7f212bd8869db81
irmtou <elee200@hotmail.com>
2024-10-25 23:12:53 -0700

    Adding many more assets
    I'm not sure if I'm supposed to add the assets that I'm only using in the game or ALL of them

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using Unity.VisualScripting;
+
+public class PlayerController : MonoBehaviour {
+    // Start variables
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Collider2D coll;
+
+    // FSM
+    private enum State { idle, walking, jumping, falling, hurt, running };
+    private State state = State.idle;
+
+    // Inspector variables
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private int shards = 0;
+    [SerializeField] private TMP_Text shardsText;
+    [SerializeField] private float hurtForce = 10f;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
+        // uitext = GetComponent<TextMeshProUGUI>();
+    }
+    private void Update() {
+
+        if (state != State.hurt) {
+            Movement(); 
+        }
+        AnimationState();
+        anim.SetInteger("state", (int)state); // Sets animation based on Enumerator state
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "Enemy") {
+            if (state == State.falling) {
+                Destroy(other.gameObject);
+                Jump();
+            }
+            else {
+                state = State.hurt;
+                if (other.gameObject.transform.position.x > transform.position.x) {
+                    // Enemy is on the right, player will be damaged and moved left
+                    // .this is implied
+                    rb.velocity = new Vector2(-hurtForce, rb.velocity.y);
+                }
+                else {
+                    // Enemy is on the left, player will be damaged and moved right
+                    rb.velocity = new Vector2(hurtForce, rb.velocity.y);
+                }
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Collectable") {
+            Destroy(collision.gameObject);
+            shards += 1;
+            shardsText.text = shards.ToString();
+        }
+    }
+
+    private void Movement() {
+        float hDirection = Input.GetAxis("Horizontal");
+
+        // Moving left
+        if (hDirection < 0) {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
+
+        }
+        // Moving right
+        else if (hDirection > 0) {
+
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+            transform.localScale = new Vector2(1, 1);
+
+        }
+        // Jumping
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground)) {
+            Jump();
+        }
+    }
+
+    private void Jump() {
+        rb.velocity = new Vector2(rb.velocity.x, 10f);
+        state = State.jumping;
+    }
+
+    private void AnimationState() {
+
+        if (state == State.jumping) {
+            if (rb.velocity.y < .1f) {
+                state = State.falling;
+            }
+        }
+        else if (state == State.falling) {
+            if (coll.IsTouchingLayers(ground)) {
+                state = State.idle;
+            }
+        }
+        else if (state == State.hurt) {
+            if (Mathf.Abs(rb.velocity.x) < .1f) {
+                state = State.idle;
+            }
+        }
+        else if (Mathf.Abs(rb.velocity.x) > 2f) {
+            // Go right
+            state = State.walking;
+        }
+        else {
+            state = State.idle;
+        }
+    }
+}
+
