968acbda51c0b58bd33e580e84c7c211af815d85
irmtou <elee200@hotmail.com>
2024-10-29 19:17:02 -0700

    Updated Top-down Player Movement
    This is big news

--- Assets/Scripts/PlayerConTD.cs
+++ Assets/Scripts/PlayerConTD.cs
@@ -1,34 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// using UnityEngine.Tilemaps;
 // using UnityEngine.InputSystem;
 
 public class PlayerConTD : MonoBehaviour {
 
-    public float moveSpeed = 3f;
+    // public float moveSpeed = 3f;
     // public float collisionOffset = 0.05f;
     // public ContactFilter2D movementFilter;
 
-    Vector2 movement;
-    private Rigidbody2D rb;
+    private Vector2 movement;
+    Rigidbody2D rb;
+    Animator anim;
+
+    public float speed = 0.5f;
+
+    private Vector2 lastMoveDirection;
+    private bool facingLeft = false;
     // List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
-        // anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
         // coll = GetComponent<Collider2D>();
     }
 
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        ProcessInputs();
+        Animate();
+        if(movement.x < 0 && !facingLeft || movement.x > 0 && facingLeft) {
+            Flip();
+        }
 
     }
 
     private void FixedUpdate() {
-        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.deltaTime);
+        rb.velocity = movement * speed;
+        // rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.deltaTime);
+    }
+
+    void ProcessInputs() {
+
+        // Store last move direction when we stop moving
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        if((moveX == 0 && moveY ==0) && (movement.x != 0 || movement.y != 0)) {
+           lastMoveDirection = movement;
+
+        }
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement.Normalize();
+    }
+
+    void Animate() {
+        anim.SetFloat("MoveX", movement.x);
+        anim.SetFloat("MoveY", movement.y);
+        anim.SetFloat("MoveMagnitude", movement.magnitude);
+        anim.SetFloat("LastMoveX", lastMoveDirection.x);
+        anim.SetFloat("LastMoveY", lastMoveDirection.y);
+    }
+
+    void Flip() {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1; // Flips the sprite
+        transform.localScale = scale;
+        facingLeft = !facingLeft;
     }
 }
